421645b4557854a0f826451e8308ac75fe9f9cbe
Andras Nemeth <andrasjanos.nemeth@ge.com>
2016-07-05 13:27:13 +0200

    ezek nemkellenek mert ebben a branchben nem látszódnak

--- app/app.js
+++ app/app.js
@@ -6,7 +6,6 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
-  'myApp.user_list',
   'myApp.change_password'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
