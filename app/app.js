6943510a404db55af536475dbf5318ff03e90bb6
Aron Horvath <aron.horvath@ge.com>
2016-07-05 14:21:09 +0200

    "US3 bug fix"

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ angular.module('myApp', [
   'userServiceModule',
   'myApp.view1',
   'myApp.view2',
-  'myApp.groups',
+  'myApp.contactGroupView',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
--- app/contact_group_view/contact_group_view.js
+++ app/contact_group_view/contact_group_view.js
@@ -4,7 +4,7 @@ angular.module('myApp.contactGroupView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/contact_group', {
-    templateUrl: 'contactGroupView/contact_group_view.html',
+    templateUrl: 'contact_group_view/contact_group_view.html',
     controller: 'GroupsCtrl'
   });
 }])
