715a9ec44b5b716622e46c759a585831b13fcca5
Miklos Toth <miklos.toth1@ge.com>
2016-07-05 17:35:42 +0200

    user_list is now tested

--- app/user_list/user_list.js
+++ app/user_list/user_list.js
@@ -1,7 +1,25 @@
 'use strict';
 
 angular.module('myApp.user_list', ['ngRoute'])
+    .service('UserService', function () {
+        return {
+            getUsers: function () {
+                var deferred = $q.deferred;
 
+                angular.timeout(function () {
+                    deferred.resolve([
+                        {"userName": "andras", "role": "ADMIN"},
+                        {"userName": "david", "role": "USER"}
+                    ]);
+                }, 500);
+
+                return deferred.promise;
+            }
+        };
+    })
+    .factory('alma', function () {
+        return {'alma': "haho"};
+    })
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/user_list', {
             templateUrl: 'user_list/user_list.html',
@@ -9,10 +27,13 @@ angular.module('myApp.user_list', ['ngRoute'])
         });
     }])
 
-    .controller('UserListController', function ($scope) {
-        $scope.users = [
-            {"username": "andras", "group": "teszt"},
-            {"username": "david", "group": "alma"},
-            {"username": "miki", "group": "banana"},
-        ];
+    .controller('UserListController', function ($scope, UserService) {
+        UserService.getUsers()
+            .then(function (users) {
+                $scope.users = users;
+            }, function (error) {
+                $scope.errors = {
+                    message: error.message
+                };
+            });
     });
---
+++ app/user_list/user_list_test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+describe('myApp.user_list', function() {
+
+  var deferred;
+  var $mockScope;
+
+  beforeEach(module('myApp.user_list'));
+  beforeEach(inject(function($controller, $rootScope, $q ){
+    deferred = $q.defer();
+    $mockScope = $rootScope.$new();
+  }));
+
+
+  describe('UserListController', function() {
+    it('should use the contactService.getUsers to retrieve the group list', inject(function($controller, $rootScope) {
+     //Given
+      var mockUsers = [{name : "Group1"},{name : "Group2"}];
+
+      var mockUserService = {
+          "getUsers" : function() {
+            deferred.resolve(mockUsers);
+            return deferred.promise;
+          }
+      };
+
+      //When
+      $controller('UserListController', {'$scope' : $mockScope, 'UserService' : mockUserService});
+
+      //Then
+      $rootScope.$apply(); // must be here in order to test resolved deferred objects
+      expect($mockScope.users).toBe(mockUsers);
+    }));
+
+    it('should populate the errors into $scope', inject(function($controller, $rootScope) {
+      //Given
+      var mockError = [{code : 403, message : "Not authorized"}];
+
+      var mockUserService = {
+        "getUsers" : function() {
+          deferred.reject(mockError);
+          return deferred.promise;
+        }
+      };
+
+      //When
+      $controller('UserListController', {'$scope' : $mockScope, 'UserService' : mockUserService});
+
+      //Then
+      $rootScope.$apply(); // must be here in order to test resolved deferred objects
+      expect($mockScope.errors.message).toBe(mockError.message);
+    }));
+  });
+});
