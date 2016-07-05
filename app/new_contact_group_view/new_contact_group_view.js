f9b60d3de705a6e3fa5091bfda2947e30636e214
Mate Juhasz <mate.juhasz@ge.com>
2016-07-05 15:50:27 +0200

    going to newly created group, todo

--- app/new_contact_group_view/new_contact_group_view.js
+++ app/new_contact_group_view/new_contact_group_view.js
@@ -2,30 +2,41 @@
 
 angular.module('myApp.new_contact_group_view', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/new_contact_group_view', {
-    templateUrl: 'new_contact_group_view/new_contact_group_view.html',
-    controller: 'newContactGroupViewCtrl'
-});
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/new_contact_group_view', {
+            templateUrl: 'new_contact_group_view/new_contact_group_view.html',
+            controller: 'newContactGroupViewCtrl'
+        });
+    }])
 
-.controller('newContactGroupViewCtrl', function($scope) {
-    console.log("newContactGroupView controller online!")
-  $scope.contactGroups = [];
-  $scope.new_contact_group = "";
-    $scope.status ="";
-
-  $scope.createNewContactGroup = function(){
-    if (!$scope.new_contact_group){$scope.status= "Name missing";return}
-    if ($scope.contactGroups.indexOf($scope.new_contact_group) == -1) {
-        $scope.contactGroups.push($scope.new_contact_group);
-        $scope.status = "New Contact Group created with name: "+$scope.new_contact_group;
-    }
-    else {
-      $scope.status = "A contact group already exists with this name: "+$scope.new_contact_group;
-    }
-
-  }
+    .controller('newContactGroupViewCtrl', function ($scope) {
+        console.log("newContactGroupView controller online!")
+        $scope.contactGroups = [];
+        $scope.new_contact_group = "";
+        $scope.status = "";
 
 
-});
+        $scope.createNewContactGroup = function () {
+
+            if (!$scope.new_contact_group) {
+                $scope.status = "Name missing";
+                return
+            }
+
+            if ($scope.contactGroups.indexOf($scope.new_contact_group) == -1) {
+                $scope.contactGroups.push($scope.new_contact_group);
+                $scope.status = "New Contact Group created with name: " + $scope.new_contact_group;
+                setTimeout($scope.goToNewlyCreatedContactGroup,1500);
+
+            }
+            else {
+                $scope.status = "A contact group already exists with this name: " + $scope.new_contact_group;
+            }
+        }
+
+        $scope.goToNewlyCreatedContactGroup = function () {
+            window.location.href = "index.html"
+        }
+
+
+    });
