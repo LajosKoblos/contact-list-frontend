405d4cc9040caaf49353e422f1091c50e385afdb
andieri <andieri@gmail.com>
2016-07-06 13:04:46 +0200

    namin convention

--- app/authentication_view/authentication_view.js
+++ app/authentication_view/authentication_view.js
@@ -2,7 +2,7 @@
 var authentication = angular.module('myApp.authentication', ['ngRoute', 'authServiceModule']);
 
 authentication.config(function ($routeProvider) {
-    $routeProvider.when('/login', {
+    $routeProvider.when('/', {
         templateUrl: '/authentication_view/login.html',
         controller: 'loginController'
     });
@@ -18,34 +18,40 @@ authentication.config(function ($routeProvider) {
 
 });
 
-authentication.run(function ($location, $rootScope) {
+authentication.run(function ($location, $rootScope, Auth) {
     $rootScope.$on("$routeChangeStart", function (event, nextRoute, currentRoute) {
-        console.log(nextRoute);
-        console.log(currentRoute);
+        console.log("nextRoute: " + nextRoute);
+        console.log("currentRoute: " + currentRoute);
         //$location.path(nextRoute).replace();
     })
 });
 
 
-authentication.controller('loginController', function ($scope, $location, $httpWithProtection, authService) {
+authentication.controller('loginController', function ($scope, $rootScope, $location, $httpWithProtection, authService) {
     $scope.user = {userName: '', password: ''};
+    /*
 
-    $scope.users = [
-        {username: "user1", password: "passwd", token: "UserToken"},
-        {username: "cheatUser", password: "cheating", token: "cheatUserToken"},
-        {username: 'admin', password: "adminpasswd", token: "adminUserToken"}
-    ];
+     $scope.users = [
+     {username: "user1", password: "passwd", token: "UserToken"},
+     {username: "cheatUser", password: "cheating", token: "cheatUserToken"},
+     {username: 'admin', password: "adminpasswd", token: "adminUserToken"}
+     ];
+     */
 
     $scope.login = function () {
+        //authService.login($scope.user)
         authService.login($scope.user.userName, $scope.user.password)
-            .then(function (response) {
-                console.log("response " + response);
-                $location.path("/users");
-                $scope.loginError = false;
-            }).catch(function (exception) {
-            console.log("exception" + exception);
-            // });
-        });
+            .then(function (result) {
+                console.log(result);
+
+                //var nextpath = (result.Role == 'USER') ? "/groups" : "/users";
+                $rootScope.userRole = '';
+                $location.path(path);
+                // $scope.loginError = false;
+            }, function (error) {
+                console.log("exception" + error);
+                $rootScope.error = error;
+            });
     }
 
     $scope.logout = function () {
