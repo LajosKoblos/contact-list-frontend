4733612b2f33b338a9ed81241c79a1ecb46eee99
David Zilahi <david.zilahi@ge.com>
2016-07-07 14:41:50 +0200

    Basic login functionality merged. Routing seems to be working.

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ require("userService");
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
 	'ngRoute',
+	'myApp.authentication',
 	'authServiceModule',
 	'contactServiceModule',
 	'contactGroupServiceModule',
@@ -18,8 +19,102 @@ angular.module('myApp', [
 	'myApp.user_list',
 	'myApp.user_creation',
 	'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+	// $locationProvider.html5Mode(true);
 	$locationProvider.hashPrefix('!');
-	$routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+	$routeProvider.otherwise({redirectTo: '/login'});
+}])
+	.factory('DefaultPathes', function () {
+		var USERDEFAULT = '/groups';
+		var ADMINDEFAULT = '/users';
+		var oldURL = '';
+
+		function setOldPath(path) {
+			oldURL = path;
+		}
+
+		function getOldPath() {
+			return oldURL;
+		}
+
+		function clean() {
+			oldURL = '';
+		}
+
+		return {
+			USER: USERDEFAULT,
+			ADMIN: ADMINDEFAULT,
+			OLD: getOldPath,
+			setOLD: setOldPath,
+			cleanOLDPath: clean,
+			hasOldPath: function () {
+				return oldURL == '';
+			}
+		}
+	})
+	.factory('RedirectFactory', ['Auth', 'DefaultPathes', function (auth, defaultPathes) {
+
+		var oldURL = defaultPathes.OLD();
+		function validateRedirect() {
+			if (!oldURL.startsWith(defaultPathes.USER) || !oldURL.startsWith(defaultPathes.ADMIN)) {
+				return auth.isLoggedIn() == 'USER' ? defaultPathes.USER : defaultPathes.ADMIN;
+			} else {
+				if (auth.isLoggedIn() == 'USER') {
+					if (oldURL.startsWith(defaultPathes.USER))
+						return defaultPathes.getOldPath();
+				}
+				if (auth.isLoggedIn() == 'ADMIN') {
+					if (oldURL.startsWith(defaultPathes.ADMIN) || oldURL.startsWith(defaultPathes.USER))
+						return defaultPathes.getOldPath();
+				}
+			}
+		}
+
+		return {
+			validate: validateRedirect
+		}
+
+	}])
+	.factory('Auth', function () {
+		var userRole;
+
+		return {
+			getRole: function () {
+				return userRole;
+			},
+			setRole: function (role) {
+				userRole = role;
+			},
+			isLoggedIn: function () {
+				return userRole ? userRole : false;
+			}
+		}
+	})
+	.run(['$rootScope', '$location', 'Auth', 'RedirectFactory', 'DefaultPathes', function ($rootScope, $location, auth, redirectFactory, defaultPathes) {
+		$rootScope.$on("$routeChangeStart", function (event, nextRoute, currentRoute) {
+
+			console.log(currentRoute);
+
+			console.log(nextRoute);
+
+			console.log("old URL");
+			console.log(defaultPathes.hasOldPath())
+			var loggedIn = auth.isLoggedIn();
+			var path;
+			if (loggedIn) {           //valaki bejelentkezve vam
+		//		path = defaultPathes.hasOldPath() ? redirectFactory.validate() : nextRoute.$$route.originalPath;
+				path = nextRoute.$$route.originalPath;
+
+			} else {      //nincs bejelentkezve
+				if ((typeof currentRoute) !== 'undefined' && (typeof currentRoute.$$route) !== 'undefined' ) {
+					defaultPathes.setOLD(currentRoute.$$route.originalPath);
+				}
+				path = "/login";
+			}
+			console.log(path);
+			$location.path(path).replace();
+			defaultPathes.cleanOLDPath();
+		});
+	}
+
+	]);
---
+++ app/authentication_view/authentication_view.js
@@ -0,0 +1,53 @@
+'use strict';
+var authentication = angular.module('myApp.authentication', ['ngRoute', 'authServiceModule']);
+authentication.config(function ($routeProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: '/authentication_view/login.html',
+        controller: 'loginController'
+    });
+    $routeProvider.when('/users', {
+        templateUrl: '/user_view/users.html',
+        controller: 'userController',
+        loginRequired: true
+    });
+    $routeProvider.when('/logout', {
+        //templateUrl: '/authentication_view/login.html',
+        controller: 'loginController'
+    });
+
+});
+authentication.controller('loginController', function ($scope, $rootScope, $location, authService, Auth) {
+    $scope.user = {userName: '', password: ''};
+
+    $scope.error = '';
+    $scope.login = function () {
+        console.log($scope.user);
+        authService.login($scope.user).then(
+         function (result) {
+             Auth.setRole(result.role);
+             var path = (result.role == 'USER') ? "/groups" : "/users";
+             $location.path(path);
+            console.log(result);
+             $scope.error = '';
+         },
+         function(error){
+             if (error.status == 401) {
+                 $scope.error = error;
+             }
+             if (error.status == 500){
+                 $scope.error = error;
+             }
+             console.log(error);
+         });
+        console.log("login");
+    }
+
+
+    $scope.logout = function () {
+        authService.logout();
+        $scope.user = {userName: '', password: ''};
+        $scope.loginError = false;
+        $scope.tokenID = '';
+        $location.path('/login');
+    }
+});
