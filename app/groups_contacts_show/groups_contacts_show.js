32eb69357b7d675b9208c611e52bc464922fe4ef
Zsolt Vad <zsoltsandor.vad@ge.com>
2016-07-07 09:42:19 +0200

    Contact sites with states (new, show, edit)

--- app/groups_contacts_show/groups_contacts_show.js
+++ app/groups_contacts_show/groups_contacts_show.js
@@ -9,18 +9,21 @@ angular.module('myApp.groupsContactsShow', ['ngRoute'])
 	}).when('/groups/:groupId/contacts', {
 		templateUrl: 'groups_contacts_show/groups_contacts_show.html',
 		controller: 'groupsContactsShowCtrl'
-	}).when('/groups/:groupId/contacts/show/:contactId', {
+	}).when('/groups/:groupId/contacts/:action', {
 		templateUrl: 'groups_contacts_show/groups_contacts_show.html',
 		controller: 'groupsContactsShowCtrl'
-	}).when('/groups/:groupId/contacts/edit/:contactId', {
+	}).when('/groups/:groupId/contacts/:action/:contactId', {
+		templateUrl: 'groups_contacts_show/groups_contacts_show.html',
+		controller: 'groupsContactsShowCtrl'
+	}).when('/groups/:groupId/contacts/:action/:contactId', {
 		templateUrl: 'groups_contacts_show/groups_contacts_show.html',
 		controller: 'groupsContactsShowCtrl'
 	});
-
 }])
 
 .controller('groupsContactsShowCtrl', function ($scope, $routeParams, $location, contactGroupService, contactService) {
 	$scope.groups = [];
+	$scope.state = ([ 'show', 'edit', 'new' ].indexOf($routeParams.action) !== -1) ? $routeParams.action : 'show';
 
 	contactGroupService.listGroups().then(function (groups) {
 		
@@ -28,21 +31,30 @@ angular.module('myApp.groupsContactsShow', ['ngRoute'])
 		$scope.contacts = [];
 		$scope.currentGroupId = (!$routeParams.groupId) ? $scope.groups[0].name : $routeParams.groupId;
 
-		contactService.getContactsInGroup($scope.currentGroupId).then(function (contacts) {
+		contactService.getContactsInGroup($scope.currentGroupId).then(function ( contacts ) {
 			// Creating 'id' property for each contact object
 			contacts.forEach( function ( contact ) {
 				contact.id = contact._links.self.href.split('/').slice(-1).pop();
 			});
 
 			$scope.contacts = contacts;
-			$scope.currentContactId = (!$routeParams.contactId) ? $scope.contacts[0].id : $routeParams.contactId;
+			$scope.currentContactId = (!$routeParams.contactId && $scope.state !== 'new') ? $scope.contacts[0].id : $routeParams.contactId;
 
-			var matchedContacts = $scope.contacts.filter( function(contact) {
+			var matchedContacts = $scope.contacts.filter( function ( contact ) {
 				return (contact.id === $scope.currentContactId);
 			});
 
 			$scope.contact = (matchedContacts.length > 0) ? matchedContacts[0] : null;
-			$location.path('/groups/' + $scope.currentGroupId + '/contacts/show/' + $scope.currentContactId);
+
+			// TODO !!!! /new/1 ?!?!?!
+			switch( $scope.state ) {
+				case 'new':
+					$location.path('/groups/' + $scope.currentGroupId + '/contacts/' + $scope.state);
+					break;
+				default:
+					$location.path('/groups/' + $scope.currentGroupId + '/contacts/' + $scope.state + '/' + $scope.currentContactId);
+			}
+			
 		});
 	});
 });
