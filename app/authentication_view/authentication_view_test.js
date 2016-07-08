96495c9db44ed2907b1126e729d8e2aa04e80445
andieri <andieri@gmail.com>
2016-07-08 09:15:54 +0200

    remove console log

--- app/authentication_view/authentication_view_test.js
+++ app/authentication_view/authentication_view_test.js
@@ -2,16 +2,63 @@
 
 describe('myApp.authentication', function() {
 
+
     var deferred;
     var $mockScope;
+    var mockAuthService;
+    var credentialsReceived
 
     beforeEach(module('myApp.authentication'));
     beforeEach(inject(function($controller, $rootScope, $q ){
         deferred = $q.defer();
         $mockScope = $rootScope.$new();
+        mockAuthService = {
+            login: function (credentials) {
+                credentialsReceived = credentials;
+                deferred.resolve({tokenId: "ABC123"});
+                return deferred.promise;
+            },
+
+            getTokenId: function () {
+                return "ABC123";
+            }
+        };
+        credentialsReceived = {};
     }));
-    
-    describe('LoginController', function() {
+    describe('loginController', function() {
+        it('should initialize $scope.user.userName and $scope.user.password with empty Strings', inject(function($controller, $rootScope) {
+            //When controller is initialized
+            var controller = $controller('loginController', {'$scope' : $mockScope});
+
+            //Then we are expecting an empty user object in the scope with empty userName and password fields
+            expect($mockScope.user).toBeDefined();
+            expect($mockScope.user.userName).toEqual("");
+            expect($mockScope.user.password).toEqual("");
+        }));
+
+        it('should call authService.login method with the credentials', inject(function($controller, $rootScope) {
+            //Given
+            var testUser = "test";
+            var testPassword = "passwd1";
+            var mockCredentials = {userName : testUser, password : testPassword};
+
+            //When controller is initialized
+            var controller = $controller('loginController', {'$scope' : $mockScope, 'authService' : mockAuthService});
+
+
+            // When user enters the credentials and selects Login
+            $mockScope.user.userName = mockCredentials.userName;
+            $mockScope.user.password = mockCredentials.password;
+            $rootScope.login();
+
+            $rootScope.$apply(); // must be here in order to test resolved deferred objects
+
+            //Then we are expecting that the authService.login method shoould be called with the credentials
+            expect(credentialsReceived).toEqual(mockCredentials);
+
+
+        }));
+/*
         it('should use the authenticationService.login to retrieve the ', inject(function($controller, $rootScope) {
             //Given
             var mockUser= [{userName : "Admin", password : "Alma1234", Role : "ADMIN"}];
@@ -47,6 +94,6 @@ describe('myApp.authentication', function() {
             //Then
             console.log($mockScope.errors);
             expect($mockScope.errors.message).toBe(mockError.message);
-        }));
+        }));*/
     });
 });
